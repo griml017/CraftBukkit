16c83f8b5c61b3fa509f104522730714274234b7
alexemmons <alex.emmons@gmail.com>
2014-04-30 21:46:49 -0500

    Replaced a switch with a HashMap.

--- src/main/java/org/bukkit/craftbukkit/CraftEffect.java
+++ src/main/java/org/bukkit/craftbukkit/CraftEffect.java
@@ -1,5 +1,7 @@
 package org.bukkit.craftbukkit;
 
+import java.util.HashMap;
+
 import org.apache.commons.lang.Validate;
 import org.bukkit.Effect;
 import org.bukkit.Material;
@@ -18,38 +20,52 @@ public class CraftEffect {
             datavalue = ((Material) data).getId();
             break;
         case SMOKE:
-            switch((BlockFace) data) { // TODO: Verify (Where did these values come from...?)
-            case SOUTH_EAST:
-                datavalue = 0;
-                break;
-            case SOUTH:
-                datavalue = 1;
-                break;
-            case SOUTH_WEST:
-                datavalue = 2;
-                break;
-            case EAST:
-                datavalue = 3;
-                break;
-            case UP:
-            case SELF:
-                datavalue = 4;
-                break;
-            case WEST:
-                datavalue = 5;
-                break;
-            case NORTH_EAST:
-                datavalue = 6;
-                break;
-            case NORTH:
-                datavalue = 7;
-                break;
-            case NORTH_WEST:
-                datavalue = 8;
-                break;
-            default:
-                throw new IllegalArgumentException("Bad smoke direction!");
-            }
+        	
+        	HashMap<BlockFace, Integer> setDataValue = new HashMap<BlockFace, Integer>();
+        	setDataValue.put(BlockFace.SOUTH_EAST, 0);
+        	setDataValue.put(BlockFace.SOUTH, 1);
+        	setDataValue.put(BlockFace.SOUTH_WEST, 2);
+        	setDataValue.put(BlockFace.EAST, 3);
+        	setDataValue.put(BlockFace.SELF, 4);
+        	setDataValue.put(BlockFace.WEST, 5);
+        	setDataValue.put(BlockFace.NORTH_EAST, 6);
+        	setDataValue.put(BlockFace.NORTH, 7);
+        	setDataValue.put(BlockFace.NORTH_WEST, 8);
+        	
+        	datavalue = setDataValue.get(data);
+        	
+//            switch((BlockFace) data) { // TODO: Verify (Where did these values come from...?)
+//            case SOUTH_EAST:
+//                datavalue = 0;
+//                break;
+//            case SOUTH:
+//                datavalue = 1;
+//                break;
+//            case SOUTH_WEST:
+//                datavalue = 2;
+//                break;
+//            case EAST:
+//                datavalue = 3;
+//                break;
+//            case UP:
+//            case SELF:
+//                datavalue = 4;
+//                break;
+//            case WEST:
+//                datavalue = 5;
+//                break;
+//            case NORTH_EAST:
+//                datavalue = 6;
+//                break;
+//            case NORTH:
+//                datavalue = 7;
+//                break;
+//            case NORTH_WEST:
+//                datavalue = 8;
+//                break;
+//            default:
+//                throw new IllegalArgumentException("Bad smoke direction!");
+//            }
             break;
         case STEP_SOUND:
             Validate.isTrue(((Material) data).isBlock(), "Material is not a block!");
